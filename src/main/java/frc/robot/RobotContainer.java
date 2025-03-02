3fa028cea40b14eec4bad48f473e967cdbbd6735
Kaya <95276965+kytpbs@users.noreply.github.com>
2025-03-02 02:00:27 +0100

    Add new elevator subsystem (#4)
    Steps to complete for this PR to actually be useful:
    
    1: Find encoder positions
    Move the elevator by hand to the height we wish to set for each L1, L2, L3, and L4. See Constants.java(L158-L161)
    
    2: System Identification
    At least find a good kG value before starting PID. Follow: https://docs.wpilib.org/en/stable/docs/software/advanced-controls/introduction/tuning-elevator.html
    
    3: PID
    Tune PID and if we still require the usage of the I term, then we did something wrong in step 2 System Identification we should set all PID back to 0 and retune the sysID

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -15,6 +15,7 @@ import frc.robot.commands.DriveCommands;
 import frc.robot.simulationSystems.PhotonSim;
 import frc.robot.subsystems.CoralSubsystem;
 import frc.robot.subsystems.DriveSubsystem;
+import frc.robot.subsystems.ElevatorSubsystem;
 import frc.robot.subsystems.LEDSubsystem;
 import frc.robot.subsystems.PhotonCameraSystem;
 import java.util.Map;
@@ -28,6 +29,7 @@ public class RobotContainer {
 
   private final DriveSubsystem driveSubsystem = new DriveSubsystem();
   private final CoralSubsystem coralSubsystem = new CoralSubsystem();
+  private final ElevatorSubsystem elevatorSubsystem = new ElevatorSubsystem();
   private final LEDSubsystem ledSubsystem = new LEDSubsystem();
 
   public RobotContainer() {
@@ -64,6 +66,8 @@ public class RobotContainer {
   private void setDefaultCommands() {
     driveSubsystem.setDefaultCommand(DriveCommands.driveWithController(driveSubsystem, controller));
     ledSubsystem.setDefaultCommand(ledSubsystem.runPattern(LEDPattern.kOff));
+    elevatorSubsystem.setDefaultCommand(
+        elevatorSubsystem.set(ElevatorSubsystem.ElevatorPosition.IDLE));
   }
 
   private void configureJoystickBindings() {
@@ -72,6 +76,9 @@ public class RobotContainer {
     commandController.rightBumper().whileTrue(coralSubsystem.takeCoral());
 
     commandController.leftBumper().whileTrue(coralSubsystem.throwCoral());
+
+    commandController.y().whileTrue(elevatorSubsystem.set(ElevatorSubsystem.ElevatorPosition.L4));
+    commandController.y().whileTrue(elevatorSubsystem.set(ElevatorSubsystem.ElevatorPosition.L2));
 
     // .start is the `start` button on the controller not a `start` function.
     commandController.start().onTrue(driveSubsystem.resetFieldOrientation());
